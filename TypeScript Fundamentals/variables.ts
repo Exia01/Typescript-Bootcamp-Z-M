3a8326ae3f5951a207fe31a7d03ecb5335d65046
Jose <gonzalezj@protonmail.com>
2024-10-30 16:40:53 -0400

    Add various TypeScript variable examples and declarations

---
+++ TypeScript Fundamentals/variables.ts
@@ -0,0 +1,50 @@
+
+ // Store the name of the course as a constant string
+const stringCourseName: string = 'Introduction to TypeScript';
+
+// Store the name of the course, but allow it to be changed later
+let courserNameLet: string = 'Introduction to TypeScript';
+
+// Store a single character 'k' as a constant
+const letterK: string = 'k';
+
+// Store a whole number that can be modified
+let numberAmount: number = 10;
+
+// Store a decimal number that can be modified
+let fractionalAmount: number = 10.5;
+
+// Store an array of numbers
+let arrayNumbers: number[] = [1, 2, 3, 4, 5];
+
+// Store the number 1000 using scientific notation
+const oneThousand = 1e3;
+
+// Store a number using octal notation (represents file permissions in Unix)
+const allPermissions = 0o777;
+
+// Store a number using hexadecimal notation (represents a byte)
+const hexByte = 0xff;
+
+// Store a number using binary notation (represents ASCII 'A')
+const binaryNumber = 0b01000001;
+
+// Store a large integer using BigInt
+const bigInteger = 9000n;
+
+// Store a boolean value representing 'true'
+const yesBool: boolean = true;
+
+// Store a boolean value representing 'false'
+const nopeBool: boolean = false;
+
+// Explicitly declare a variable as undefined (use sparingly)
+const missingVariable: undefined = undefined;
+
+// Explicitly declare a variable as null (use sparingly)
+const empty: null = null;
+
+// Declare a variable without specifying type or initial value (avoid if possible)
+let hello;
+
+//
