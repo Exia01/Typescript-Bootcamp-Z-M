b3921ef18ba778ffc8f09a847d551175cc0542e7
Jose <gonzalezj@protonmail.com>
2024-11-08 16:24:39 -0500

    add switchCase  example

--- TypeScript Fundamentals/controlflow.ts
+++ TypeScript Fundamentals/controlflow.ts
@@ -7,3 +7,23 @@ const isFiscalYearResetMarker = (month: string): boolean => {
     }
     return false;
 };
+
+//using switch statement
+const evaluateIfFruitVegetableOrLegume = (fruit: string): string => {
+    switch (fruit.toLowerCase()) {
+        case 'apple':
+        case 'banana':
+        case 'orange':
+            return 'fruit';
+        case 'broccoli':
+        case 'carrot':
+        case 'cauliflower':
+            return 'vegetable';
+        case 'pepper':
+        case 'onion':
+        case 'garlic':
+            return 'legume';
+        default:
+            return 'unknown';
+    }
+};
